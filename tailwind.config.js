aac94564c5132e757424ecb9e433a2105be22229
wacho91 <criskol.71@gmail.com>
2024-10-23 10:01:10 -0500

    Configuration tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        parisienne: ["Parisienne", "cursive"],
+      },
+      colors: {
+        primary: "#00C2FF",
+        secondary: "#DD0BFF",
+        dark: "#111111",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
   },
   plugins: [],
 }
