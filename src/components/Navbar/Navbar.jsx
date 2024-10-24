8303f408614cdb1c4c79fff853e46078865a4cd2
wacho91 <criskol.71@gmail.com>
2024-10-24 10:09:09 -0500

    Arquitecture navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,12 +26,36 @@ const NavLinks = [
 
 const Navbar = () => {
   return (
-    <div>
-        <div>
-            <div>
-                
-            </div>
+    <div className="relative z-[9999] text-black dark:text-white duration-300">
+      <div className="container py-2 md:py-0">
+        <div className="flex justify-between items-center">
+          {/*Logo Section*/}
+          <div className="flex items-center gap-3">
+            <img src={Logo} alt="" className="h-16"/>
+            <p className="text-3xl">
+              VR <span className="font-boldd">World</span>
+            </p>
+          </div>
+          {/*Desktop Menu Section*/}
+          <nav className="hidden md:block">
+            <ul className="flex items-center gap-8">
+              {NavLinks.map(({id, name, link}) => {
+                return (
+                  <li key={id} className="py-4">
+                    <a 
+                      href={link}
+                      className="text-xl font-semibold hover:text-primary py-2 hover:border-b-2 hover:border-secondary trasnsition-color duration-500"
+                    >
+                      {name}
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+            {/*Dark Mode*/}
+          </nav>
         </div>
+      </div>
     </div>
   )
 }
