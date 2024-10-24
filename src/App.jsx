f9022d7db801c35164b0a9f6d019f177d1151a8c
wacho91 <criskol.71@gmail.com>
2024-10-24 11:17:05 -0500

    Configuration animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { useEffect, useState } from "react";
 import Hero from "./components/Hero/Hero"
 import Navbar from "./components/Navbar/Navbar"
 
+import AOS from "aos";
+import "aos/dist/aos.css";
+
 function App() {
+
+  const [isPlay, setIsPlay] = useState(false);
+
+  const togglePlay = () => {
+    setIsPlay(!isPlay);
+  }
+
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sin",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
 
   return (
     <main className="overflow-x-hidden bg-white dark:bg-black text-black dark:text-white duration-300">
       <Navbar />
-      <Hero />
+      <Hero togglePlay={togglePlay}/>
     </main>
   )
 }
