84016d656623a8e218faffff867a6add74d0613c
wacho91 <criskol.71@gmail.com>
2024-10-24 10:36:44 -0500

    Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero/Hero"
 import Navbar from "./components/Navbar/Navbar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <main className="overflow-x-hidden bg-white dark:bg-black text-black dark:text-white duration-300">
       <Navbar />
+      <Hero />
     </main>
   )
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+
+import HeroPng from "../../assets/hero.png";
+
+const Hero = () => {
+  return (
+    <div>
+        Hero component
+    </div>
+  )
+}
+
+export default Hero
