0a3546515f72e170e4eb178087c1ad2b9d5ce086
wacho91 <criskol.71@gmail.com>
2024-10-25 10:15:06 -0500

    Banner Arquitecture

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,8 +1,44 @@
 
+import BannerPng from "../../assets/banner1.png";
+
 const Banner = () => {
   return (
     <div>
-        Banner Component
+        <div>
+            <div>
+                {/* image section */}
+                <div>
+                    <img src={BannerPng} alt="" />
+                </div>
+                {/* text content section */}
+                <div>
+                    <div>
+                        <h1>
+                            GET READY TO ENJ0Y VR{" "}
+                            <span>
+                                Movies WITH OUR PLATFORM
+                            </span>
+                        </h1>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab
+                            molestias quia exercitationem ex inventore labore beatae cumque
+                            et quaerat voluptas?
+                        </p>
+                        <div>
+                            <button>
+                                Get Started
+                            </button>
+                            <button>
+                                {" "}
+                                See More 
+                            </button>
+                        </div>
+                    </div>
+                    {/* backgrond color blob */}
+                    <div></div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
