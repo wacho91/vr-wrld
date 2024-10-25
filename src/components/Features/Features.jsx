875530c98d151bb7a1adcc3f29c34ce39a1d6e9c
wacho91 <criskol.71@gmail.com>
2024-10-25 11:23:32 -0500

    Animation Features complete

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -36,7 +36,10 @@ const Features = () => {
   return (
     <div className="container py-14 sm:min-h-[600px]">
         <div>
-            <h1 className="text-3xl font-semibold text-center sm:text-4xl mb-12">
+            <h1
+                data-aos="fade-up" 
+                className="text-3xl font-semibold text-center sm:text-4xl mb-12"
+            >
                 Why Choose Us
             </h1>
             {/* card section */}
@@ -44,6 +47,8 @@ const Features = () => {
                 {FeaturesData.map((data, index) => (
                     <div 
                         key={index}
+                        data-aos="fade-up"
+                        data-aos-delay={data.aosDelay}
                         className="text-center group space-y-3 sm:space-y-6 p-4 sm:py-10 bg-dark hover:bg-gradient-to-r from-primary to-secondary hover:shadow-[0_0_40px_#007cfff0] text-white hover:text-black rounded-lg duration-300"
                     >
                         <div className="grid place-items-center">{data.icon}</div>
