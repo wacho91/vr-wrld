ee28a9104249b80b137a7eda8f4dc7a7d876e1f2
wacho91 <criskol.71@gmail.com>
2024-10-28 09:48:32 -0500

    Animation AppStore Complete

--- src/components/AppStore/AppStore.jsx
+++ src/components/AppStore/AppStore.jsx
@@ -6,22 +6,29 @@ const AppStore = () => {
   return (
     <div className="container py-10 sm:min-h-[400px] sm:grid sm:place-items-center">
         <div className="space-y-6 max-w-xl mx-auto">
-            <h1 className="text-2xl text-center sm:text-4xl font-semibold">
+            <h1
+                data-aos="fade-up" 
+                className="text-2xl text-center sm:text-4xl font-semibold"
+            >
                 Get Started with our app
             </h1>
-            <p className="text-center sm:px-20">
+            <p
+                data-aos="fade-up"
+                data-aos-delay="300" 
+                className="text-center sm:px-20"
+            >
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
                 magnam harum accusantium odit?
             </p>
             <div className="flex flex-wrap justify-center gap-4 items-center">
-                <a href="#">
+                <a href="#" data-aos="fade-up" data-aos-delay="500">
                     <img 
                         src={AppStoreImg} 
                         alt="img"
                         className="max-w-[150px] sm:max-w-[120px] md:max-w-[200px]" 
                     />
                 </a>
-                <a href="#">
+                <a href="#" data-aos="fade-up" data-aos-delay="700">
                     <img 
                         src={PlayStoreImg} 
                         alt="img"
