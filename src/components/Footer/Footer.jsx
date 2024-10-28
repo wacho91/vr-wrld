b3eb2a717dd764442583a7dec866c8d2786d2ac0
wacho91 <criskol.71@gmail.com>
2024-10-28 10:31:27 -0500

    Footer Component Complete

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import { FaFacebook, FaInstagram, FaLinkedin, FaLocationArrow, FaMobileAlt } from "react-icons/fa";
 
 const FooterLinks = [
     { 
@@ -24,18 +25,105 @@ const FooterLinks = [
 
 const Footer = () => {
   return (
-    <div>
-        <div>
-            <div>
+    <div className="bg-gray-100 dark:bg-dark mt-14 rounded-t-3xl">
+        <div className="container">
+            <div className="grid md:grid-cols-3 py-4">
                 {/*Company Details*/}
-                <div>
-                    <h1>
+                <div className="py-8 px-4">
+                    <h1 className="sm:text-3xl text-xl font-bold sm:text-left text-justify mb-3 flex items-center gap-3">
                         Metaverse
                     </h1>
                     <p className="text-sm">
                         Lorem, ipsum dolor sit amet consectetur adipisicing elit.
                         Repellat dolor illum a aut iste expedita.
                     </p>
+                    <br />
+                    {/* contact section */}
+                    <div>
+                        <div className="flex items-center gap-3">
+                            <FaLocationArrow />
+                            <p>Noida, Uttar Pradesh</p>
+                        </div>
+                        <div className="flex items-center gap-3">
+                            <FaMobileAlt />
+                            <p>+91 123456789</p>
+                        </div>
+                    </div>
+                    {/* social handle */}
+                    <div className="flex items-center gap-3 mt-6">
+                        <a href="#">
+                            <FaFacebook className="text-3xl hover:text-primary duration-300"/>
+                        </a>
+                        <a href="#">
+                            <FaInstagram className="text-3xl hover:text-primary duration-300"/>
+                        </a>
+                        <a href="#">
+                            <FaLinkedin className="text-3xl hover:text-primary duration-300"/>
+                        </a>
+                    </div>
+                </div>
+                {/* Footer Links section */}
+                <div className="grid grid-cols-2 sm:grid-cols-3 col-span-2 md:pl-10">
+                    <div>
+                        <div className="py-8 px-4">
+                            <h1 className="text-xl font-bold mb-3">Quick Links</h1>
+                            <ul className="space-y-3">
+                                {FooterLinks.map((link) => (
+                                    <li 
+                                        key={link.id}
+                                        className="hover:translate-x-1 duration-300"
+                                    >
+                                        <a 
+                                            href={link.link}
+                                            className="cursor-pointer hover:text-primary"
+                                        >
+                                            {link.title}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="py-8 px-4">
+                            <h1 className="text-xl font-bold mb-3 ">Quick Links</h1>
+                            <ul className="space-y-3">
+                                {FooterLinks.map((link) => (
+                                <li
+                                    key={link.id}
+                                    className="hover:translate-x-1 duration-300 "
+                                >
+                                    <a
+                                    href={link.link}
+                                    className="cursor-pointer hover:text-primary"
+                                    >
+                                    {link.title}
+                                    </a>
+                                </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="py-8 px-4">
+                            <h1 className="text-xl font-bold mb-3">Location</h1>
+                            <ul className="space-y-3">
+                                {FooterLinks.map((link) => (
+                                <li
+                                    key={link.id}
+                                    className="hover:translate-x-1 duration-300 "
+                                >
+                                    <a
+                                    href={link.link}
+                                    className="cursor-pointer hover:text-primary"
+                                    >
+                                    {link.title}
+                                    </a>
+                                </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
